ae156a1a5461f7729dce4e0e20ba8930f7bd3b56
leosban <leonautcco@gmail.com>
2022-02-11 18:42:17 -0300

    Aula 4 - Principio da substituicao de Liskov

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 // Código será criado sem principios
 // Ao longo das aulas iremos corrigindo
 
-// Open/Closed Principle
-// Principio do aberto e fechado: entidades, classes, módulos, funções, métodos
-// devem estar abertas para extensões e fechadas para modificações;
-// Siginifica que não devemos ficar mexendo no nosso source code
+/* Open/Closed Principle
+Principio do aberto e fechado: entidades, classes, módulos, funções, métodos
+devem estar abertas para extensões e fechadas para modificações;
+Siginifica que não devemos ficar mexendo no nosso source code
+ */
+
+/* Principio da substituição de Liskov
+Descrição complexa, porém de fácil entendimento 
+
+
+
+*/
