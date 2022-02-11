ad27bd7bb58f950afddc3594c00c435abde7e560
leosban <leonautcco@gmail.com>
2022-02-11 17:37:55 -0300

    Aula 3 - Principio Open/Closed

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 // Código será criado sem principios
 // Ao longo das aulas iremos corrigindo
+
+// Open/Closed Principle
+// Principio do aberto e fechado: entidades, classes, módulos, funções, métodos
+// devem estar abertas para extensões e fechadas para modificações;
+// Siginifica que não devemos ficar mexendo no nosso source code
